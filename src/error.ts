4ad053b9945daa9e2a6b3813915552da2d3be838
Ian Silva <raspincel@gmail.com>
2025-02-11 21:21:57 -0300

    :))))

--- src/error.ts
+++ src/error.ts
@@ -24,3 +24,5 @@ export async function errorHandler(err: Error, req: Request, res: Response, next
   console.log(err);
   res.status(500).json({ message: 'Error interno do servidor.' });
 }
+
+// :)))
